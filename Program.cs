eff1fc2e6f9479577f36ebbff5d5c305cdce5949
murali1125 <murali1125s@gmail.com>
2020-03-31 13:13:06 +0530

    UC6: max hourse = 100, max days = 20 ; condition added

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ UC1: Start with Displaying welcome to Employee Wage Computation Program on Maste
 UC2: Calculate Daily Employee Wage assume per hour wage =20 daily working hours 8
 UC3: Add Part time Employee & Wage - Assume Part time Hour is 4
 UC4: Solving using Switch Statement
-UC5:  Calculating Wages for a Month  Assume 20 Working Day per Month
+UC5: Calculating Wages for a Month  Assume 20 Working Day per Month
+UC6: Calculate Wages till a condition of total working hours or days is reached for a month - Assume 100 hours 
 */
 
 
@@ -13,27 +14,38 @@ namespace EmployeeWage
 {
     class Program
     {
-        
+        static int days=20 ,
+                   hours=0;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage");
             // create object for Program class
             Program obj_program = new Program();
             // call EmployeeWage function
-            obj_program.MontlyEmpWage();
+            Program.MontlyEmpWage();
         }// end :  Main(string[] args)
 
 
         // method for montly employee wage calculation
-        public void MontlyEmpWage() {
+        public static void MontlyEmpWage() {
             // monthly wage variable
-            int total_monthly_wage = 0;
-            // cal 20 times Employeedailywage
-            for (int i = 1; i <= 20; i++) {
+            int total_monthly_wage = 0,
+                maxdays=days;
+
+            // cal Employeedailywage upto number of days times
+            for (int i = 1; i <=maxdays; i++) {
                 total_monthly_wage = total_monthly_wage + Program.EmployeeDailyWage(i);
+                // break the loop  if hourse reached to 100 or days reached 20
+                if (hours >= 100)
+                {
+                    days = i;
+                    break;
+                }//end : if (hours >= 100)
             }//end: for (int i = 1; i <= 20; i++)
 
             Console.WriteLine("Total monthly wage = " + total_monthly_wage);
+            Console.WriteLine("Toal working hourse in the month " + hours);
+            Console.WriteLine("Total working days in the month " + days);
         }//end: MontlyEmpWage()
 
 
@@ -51,13 +63,30 @@ namespace EmployeeWage
             // generating random number if the number=  0 absent ,  1 fulltime_present, 2 parttime_present
             Random random = new Random();
             attandance = random.Next(0,3);
-            Console.WriteLine(attandance);
             switch (attandance) {
                 case 1:
-                    temp_hours = fullDay_hours;
+                    if (hours + fullDay_hours > 100)
+                    {
+                        temp_hours = 100 - hours;
+                        hours = 100;
+                    }// end: if (hours + fullDay_hours > 100)
+                    else
+                    {
+                        hours += fullDay_hours;
+                        temp_hours = fullDay_hours;
+                    }// end : else -> if (hours + fullDay_hours > 100)
                     break;
                 case 2:
-                    temp_hours = parttime_hours;
+                    if (hours + parttime_hours > 100)
+                    {
+                        temp_hours = 100 - hours;
+                        hours = 100;
+                    }//end :  if (hours + parttime_hours > 100)
+                    else
+                    {
+                        hours += parttime_hours;
+                        temp_hours = parttime_hours;
+                    }// end: else -> if (hours + parttime_hours > 100)
                     break;
             }//end:switch (attandance)
             total_day_wage = temp_hours * wage_per_hour;
