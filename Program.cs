a04a8d533a4ff06bf0a5d004e53e0d51bef10544
murali1125 <murali1125s@gmail.com>
2020-03-31 11:21:56 +0530

    UC1: Check Employee is Present or Absent

--- Program.cs
+++ Program.cs
@@ -10,6 +10,14 @@ namespace EmployeeWage
         {
             Console.WriteLine("Welcome to Employee Wage");
         }
+        public void EmployeeWage()
+        {
+            // Variables 
+            int attandance;
+            // generating random number if the number=  0 absent ,  1 present
+            Random random = new Random();
+            attandance = random.Next(0, 2);
+        }
               
     }
 }
