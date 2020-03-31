88afb79ec21da0ee2cef73a9963c7cb01ea07be7
murali1125 <murali1125s@gmail.com>
2020-03-31 11:53:06 +0530

    UC3: Added Part time Employee & Wage

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Text;
 /* 
 UC1: Start with Displaying welcome to Employee Wage Computation Program on Master Branch
 UC2: Calculate Daily Employee Wage assume per hour wage =20 daily working hours 8
+UC3: Add Part time Employee & Wage - Assume Part time Hour is 4
 */
 
 
@@ -14,24 +15,31 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage");
-        }
+            // create object for Program class
+            Program obj_program = new Program();
+            // call EmployeeWage function
+            obj_program.EmployeeWage();
+        }// end :  Main(string[] args)
         public void EmployeeWage()
         {
             // Variables 
             int attandance,
                 wage_per_hour=20,
                 fullDay_hours=8,
-                total_day_wage;
+                parttime_hours=4,
+                total_day_wage=0,
+                temp_hours=0;
 
-            // generating random number if the number=  0 absent ,  1 present
+            // generating random number if the number=  0 absent ,  1 fulltime_present, 2 parttime_present
             Random random = new Random();
-            attandance = random.Next(0, 2);
-            
+            attandance = random.Next(0,3);
+            Console.WriteLine(attandance);
             if(attandance == 1)
-            {
-                total_day_wage = fullDay_hours * wage_per_hour;
-
-            }//end: if(attandance == 1)
+            { temp_hours = fullDay_hours; }//end: if(attandance == 1)
+            else if(attandance == 2 )
+            { temp_hours = parttime_hours; } // end: else if(attandance ==2 )
+            total_day_wage = temp_hours * wage_per_hour;
+            Console.WriteLine(total_day_wage);
         }// end:  EmployeeWage()
     } // end: class Program
 }// end : namespace EmployeeWage
